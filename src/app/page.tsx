d74b7111d656fb398061d05075b9f17794edbf13
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-02-28 21:27:37 -0600

    agregando metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,22 @@ import Boletin from '@/components/boletin/Boletin'
 import MainHeader from '@/components/mainHeader/MainHeader'
 import Services from '@/components/servicios/Services'
 import WhyUs from '@/components/whyUs/WhyUs'
+import { Metadata } from 'next'
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'Electroser | Electricidad para tu hogar o negocio',
+  description: 'Servicios de electricidad para tu hogar o negocio',
+  keywords: `electricidad, electricista, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, electricidad para el negocio, electricidad para empresas, electricidad para el hogar, 
+  electricidad para el negocio, electricidad para empresas, automatización, automatización de procesos, calidad, calidad de la energía`,
+  openGraph: {
+    title: 'Electroser | Electricidad para tu hogar o negocio',
+    description: 'Servicios de electricidad para tu hogar o negocio',
+    url: 'https://servicios-electricos.juliosanic.tech',
+    type: 'website',
+   
+  }
+} 
 
 export default function HomePage() {
   return (
